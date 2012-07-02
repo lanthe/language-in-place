820d6689eb14182dac4bf49c009d7681769bdead
lanthe <iolanthe.chronis@gmail.com>
2012-07-02 14:29:19 -0700

    [ikc] little flag buttons to change language
    aw.

--- header.php
+++ header.php
@@ -7,6 +7,7 @@ function get_header() {
 	<head>
 	    <title> test </title>
 	    <link rel='stylesheet' type='text/css' href='lip.css'>
+	    <link rel='stylesheet' type='text/css' href='flags.css'>	    
 	    <script type='text/javascript' src='http://code.jquery.com/jquery-1.7.2.js'></script>
 	    <meta name='google' value='notranslate'>  <!-- don't want chrome's message saying 'do you want this page translated' -->
 	    <meta http-equiv='Content-Language' content='en_US' /> 
@@ -20,7 +21,10 @@ function get_header() {
 			<img class='header_logo' src='lip-logo.png'></img>
 		</a>
 		<a class='header_link' href='not-implemented.php'>Login</a>
-		<a class='header_link' href='not-implemented.php'>French to English</a>		
+		<a class='header_link' href='not-implemented.php'>
+		  <img src='blank.gif' class='flag flag-us'/> &nbspto&nbsp 
+		  <img src='blank.gif' class='flag flag-fr'/>
+		</a>		
 	<div class='change_language_button'></div>
 </div>";
 }
