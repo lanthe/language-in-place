fa5689c80f9ab09f890eeb1a03ee21b5c8beb83e
lanthe <iolanthe.chronis@gmail.com>
2012-07-05 20:00:48 -0700

    [ikc] add google analytics

--- header.php
+++ header.php
@@ -14,6 +14,21 @@ function get_header() {
 	    <script type='text/javascript' src='http://code.jquery.com/jquery-1.7.2.js'></script>
 	    <meta name='google' value='notranslate'>  <!-- don't want chrome's message saying 'do you want this page translated' -->
 	    <meta http-equiv='Content-Language' content='en_US' /> 
+		<script type='text/javascript'>
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-33201912-1']);
+		  _gaq.push(['_setDomainName', 'languageinplace.com']);
+		  _gaq.push(['_setAllowLinker', true]);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
 	</head>
 
 	<body>
