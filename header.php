b8274f5f898fbd490bb1c7e4ecad17da4f99a62d
lanthe <iolanthe.chronis@gmail.com>
2013-05-16 18:31:20 -0700

    [ikc] again

--- header.php
+++ header.php
@@ -17,21 +17,7 @@ Transitional//EN\">
 	    <meta name='google' value='notranslate'>  <!-- don't want chrome's message saying 'do you want this page translated' -->
 	    <meta http-equiv='Content-Language' content='en_US' /> 
 
-<script type='text/javascript'>
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-33201912-1']);
-  _gaq.push(['_setDomainName', 'languageinplace.com']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 	</head>
 
 	<body>
